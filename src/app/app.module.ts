fde78ed2486e5d1f0995de47a8c8cc52de922019
esthernavarro <contact.esthernavarro@gmail.com>
2020-07-05 13:13:13 -0400

    removed nearby component. not needed in this app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { ChartModule } from 'angular-highcharts';
 
 import { AppComponent } from './app.component';
 import { ClockComponent } from './clock/clock.component';
 import { ChartcontainerComponent } from './chartcontainer/chartcontainer.component';
-import { NearbyComponent } from './nearby/nearby.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClockComponent,
     ChartcontainerComponent,
-    NearbyComponent
   ],
   imports: [
     BrowserModule,
-    // ChartModule // add ChartModule to your imports
   ],
   providers: [],
   bootstrap: [AppComponent]
